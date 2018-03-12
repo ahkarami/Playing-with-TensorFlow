12dc8c4df8c9f574f3b62dc1b886d424e5215185
ahkarami <amir.k1369@gmail.com>
2018-03-12 11:28:03 +0330

    Update 00_MNISTDataset.py

--- 00_MNISTDataset.py
+++ 00_MNISTDataset.py
@@ -29,4 +29,15 @@ x_test = mnist.test.images
 y_test = mnist.test.labels
 
 
+# ***** Section2 *****
+
+print('\n ***** Section2 ***** ')
+
+# Data shape:
+# MNIST data set has 55,000 examples for training and 10,000 examples for testing. Each image has a size of 784 (28x28).
+
+print("x_train: ", x_train.shape)
+print("y_train: ", y_train.shape)
+print("x_test: ", x_test.shape)
+print("y_test: ", y_test.shape)
 
