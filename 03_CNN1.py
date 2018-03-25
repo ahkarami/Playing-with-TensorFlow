157106b8cf6f826214a65665d5df2909f7911fbc
ahkarami <amir.k1369@gmail.com>
2018-03-25 11:32:01 +0430

    Update 03_CNN1.py

--- 03_CNN1.py
+++ 03_CNN1.py
@@ -23,3 +23,44 @@ print("y_train: ", y_train.shape)
 print("x_test: ", x_test.shape)
 print("y_test: ", y_test.shape)
 
+
+# ***** Section2 *****
+
+print('\n ***** Section2 ***** ')
+
+# Parameter setting:
+
+# Parameters
+learning_rate = 0.001
+training_iters = 200000
+batch_size = 100
+display_step = 200
+
+# Network parameters
+n_input = 784
+n_classes = 10
+dropout = 0.5
+
+# Graph input
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)  # to keep dropout probability
+
+weights = {
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    # 5x5 conv, 32 inputs, 64 outputs
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    # fully connected, 7*7*64 inputs, 1024 outputs
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    # 1024 inputs, 10 outputs (class prediction)
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))
+}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
