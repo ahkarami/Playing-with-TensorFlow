9bbbd062fe8a90632b3920e76514f58667aa7dbd
ahkarami <amir.k1369@gmail.com>
2018-04-01 14:19:39 +0430

    Add files via upload

---
+++ 04_SupportGPU.py
@@ -0,0 +1,27 @@
+# Besmei Taala
+# Amir Hossein Karami 
+# MainLink: https://stackoverflow.com/questions/38009682/how-to-tell-if-tensorflow-is-using-gpu-acceleration-from-inside-python-shell
+
+
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+
+# Version of TensorFlow:
+print(tf.__version__)  # e.g., 1.4.1
+
+
+# ***** Section1 *****
+
+print('\n ***** Section1 ***** ')
+
+# Approach1:
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+with tf.Session() as sess:
+    print(sess.run(c))
+
+	
